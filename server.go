973174ed2a225083430c6fc2866938b41ec7550c
Sataapon Naowat <sataapon.n@kbtg.tech>
2020-09-26 10:23:06 +0700

    dynamic PORT

--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -12,8 +13,12 @@ func main() {
 	fmt.Println("hello hometic : I'm Gopher!!")
 	r := mux.NewRouter()
 	r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
+
+	addr := fmt.Sprintf("0.0.0.0:%s", os.Getenv("PORT"))
+	fmt.Println(addr)
+
 	server := http.Server{
-		Addr:    "127.0.0.1:2009",
+		Addr:    addr,
 		Handler: r,
 	}
 	log.Println("starting...")
