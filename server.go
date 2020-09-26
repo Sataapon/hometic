05e747d4ddbe8c491bed3900c66db942c810484f
Sataapon Naowat <sataapon.n@kbtg.tech>
2020-09-26 09:30:47 +0700

    add test for PairDeviceHandler

--- server.go
+++ server.go
@@ -11,13 +11,15 @@ import (
 func main() {
 	fmt.Println("hello hometic : I'm Gopher!!")
 	r := mux.NewRouter()
-	r.HandleFunc("/pair-device", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`{"status":"active"`))
-	}).Methods(http.MethodPost)
+	r.HandleFunc("/pair-device", PairDeviceHandler).Methods(http.MethodPost)
 	server := http.Server{
 		Addr:    "127.0.0.1:2009",
 		Handler: r,
 	}
 	log.Println("starting...")
 	log.Fatal(server.ListenAndServe())
+}
+
+func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`{"status":"active"`))
 }
---
+++ server_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCreatePairDevice(t *testing.T) {
+	req := httptest.NewRequest(http.MethodPost, "/pair-device", nil)
+	rec := httptest.NewRecorder()
+
+	PairDeviceHandler(rec, req)
+
+	expected := `{"status":"active"`
+	if rec.Body.String() != expected {
+		t.Errorf("expected %q but got %q\n", expected, rec.Body.String())
+	}
+
+}
