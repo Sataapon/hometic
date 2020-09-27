a6d2d0e33c7a71ac6b755dc5a5e061ad5e81e51a
Sataapon Naowat <sataapon.n@kbtg.tech>
2020-09-27 10:01:09 +0700

    read data from request body

--- server_test.go
+++ server_test.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestCreatePairDevice(t *testing.T) {
-	req := httptest.NewRequest(http.MethodPost, "/pair-device", nil)
+	payload := new(bytes.Buffer)
+	json.NewEncoder(payload).Encode(Pair{DeviceID: 1234, UserID: 4433})
+
+	req := httptest.NewRequest(http.MethodPost, "/pair-device", payload)
 	rec := httptest.NewRecorder()
 
 	PairDeviceHandler(rec, req)
 
-	expected := `{"status":"active"`
+	expected := `{"status":"active"}`
 	if rec.Body.String() != expected {
 		t.Errorf("expected %q but got %q\n", expected, rec.Body.String())
 	}
